84c6b5dfa6e958c6aaaf90546f69fa213e5bac51
Norbert Stüken <norbert.stueken@nrbrt.com>
2016-03-21 18:58:18 +0100

    chore: Add templates and static files dirs to settings

--- marcador/marcador/settings.py
+++ marcador/marcador/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'marcador.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -87,7 +87,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -100,3 +100,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
