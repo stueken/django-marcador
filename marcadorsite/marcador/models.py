8fa7df6dae4a75c1bacbdda4de76e82b1fa29c94
Norbert Stüken <norbert.stueken@nrbrt.com>
2016-03-23 23:47:53 +0100

    feat: Add fields to models

--- marcadorsite/marcador/models.py
+++ marcadorsite/marcador/models.py
@@ -1,3 +1,19 @@
+# encoding: utf-8
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class Bookmark(models.Model):
+    url = models.URLField()
+    title = models.CharField('title', max_length=255)
+    description = models.TextField('description', blank=True)
+    is_public = models.BooleanField('public', default=True)
+    date_created = models.DateTimeField('date created')
+    date_updated = models.DateTimeField('date updated')
+    owner = models.ForeignKey(User, verbose_name='owner',
+        related_name='bookmarks')
+    tags = models.ManyToManyField(Tag, blank=True)
