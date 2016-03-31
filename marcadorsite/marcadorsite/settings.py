10baeb6f42adcc1774fc54253356da28433bfe0c
Norbert Stüken <norbert.stueken@nrbrt.com>
2016-03-31 17:51:37 +0200

    fix: Configure static and template dirs

--- marcadorsite/marcadorsite/settings.py
+++ marcadorsite/marcadorsite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'marcadorsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -88,7 +88,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -101,3 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
