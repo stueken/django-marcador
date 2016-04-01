4a628ab7b422e59c47fdfe6ec0b406360e7c78eb
Norbert Stüken <norbert.stueken@nrbrt.com>
2016-04-01 21:38:52 +0200

    feat: Add URLS for the forms

--- marcadorsite/marcador/urls.py
+++ marcadorsite/marcador/urls.py
@@ -4,5 +4,9 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^user/(?P<username>[-\w]+)/$', 'marcador.views.bookmark_user',
         name='marcador_bookmark_user'),
+    url(r'^create/$', 'marcador.views.bookmark_create',
+        name='marcador_bookmark_create'),
+    url(r'^edit/(?P<pk>\d+)/$', 'marcador.views.bookmark_edit',
+        name='marcador_bookmark_edit'),
     url(r'^$', 'marcador.views.bookmark_list', name='marcador_bookmark_list'),
 ]
